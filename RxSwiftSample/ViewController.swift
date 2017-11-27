64e9ad41164469439ac331e6a4f66efb9dcf6aa2
Morita Jun <moritajun@Morita-no-MacBook-Pro.local>
2017-11-28 00:51:19 +0900

    add new file

--- RxSwiftSample/ViewController.swift
+++ RxSwiftSample/ViewController.swift
@@ -7,19 +7,31 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+
+// unsubscribeに必要なもの？
+let disposeBug = DisposeBag()
 
 class ViewController: UIViewController {
 
+    @IBOutlet private weak var label: UILabel!
+    @IBOutlet private weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        textField.rx.text
+            .map{$0} // observerを１個ずつ処理するよ
+            .bind(to: label.rx.text) // observerに紐づける処理
+            .disposed(by: disposeBug) // いらなくなったら解放するよ
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
